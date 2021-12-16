ab198e73dbb06ebdc08f82d1029ecca6c359e5cd
TS22082 <ts22082@gmail.com>
2021-12-16 11:20:50 -0800

    chore(refactor): string with special chars wont work as url id, set up for form

--- client/src/pages/Lead/Lead.js
+++ client/src/pages/Lead/Lead.js
@@ -1,20 +1,13 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
 import axios from "axios";
+import "./lead.css";
 
 const Lead = () => {
-  const { id } = useParams();
-
   useEffect(() => {
-    console.log(id);
     axios.get("/api/appr4leads").then((res) => console.log("result ===>", res));
   }, []);
 
-  return (
-    <div>
-      <h1>hello world</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Lead;
